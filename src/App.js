c2187ac96454a20c5d3e283f2f3f7bb51e123e6c
yourname <ankitaa2229@gmail.com>
2023-02-11 14:54:47 +0530

    modification

--- src/App.js
+++ src/App.js
@@ -7,8 +7,33 @@ function App() {
     <div className='container'>
       
      <h1>BookList Maintainance Form</h1>
-    
-    
+     <h2>Add and view your books using local storage</h2>
+
+     
+
+    <div className='form'>
+     
+      <div className='form_1'>
+        <label>Title  </label>
+        <input type="text" name="title"></input>
+      </div>
+
+      <div className='form_2'>
+        <label>Author</label>
+        <input type="text" name="author"></input>
+      </div>
+
+      <div className='form_3'>
+        <label>ISBN# </label>
+        <input type="text" name="isbn#"></input>
+      </div>
+      
+      <div>
+        <button type='submit'>ADD</button>
+        </div>
+
+    </div>
+
     </div>
     
     
